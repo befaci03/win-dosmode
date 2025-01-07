6dd107d404604141474156618c6acb12e3bf1a68
Befaci <145709984+befaci03@users.noreply.github.com>
2025-01-07 21:22:32 +0100

    Error message when enable

---
+++ enablescript.cmd
@@ -0,0 +1,6 @@
+@echo off
+cls
+color 4
+echo [ERROR] Operation was canceled: The DOS mode functionnality project has been not started, please retry later
+echo [INFO] You can check it at https://github.com/befaci03/win-dosmode
+pause
